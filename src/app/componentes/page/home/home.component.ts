4660ad08fd1f5381ab18e47caa6c4d1fde0d959b
Brayan Betancourt Ramirez <braybeta@espol.edu.ec>
2024-11-18 17:04:28 -0500

    empezado galeria

--- src/app/componentes/page/home/home.component.ts
+++ src/app/componentes/page/home/home.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { PlanesComponent } from "../shared/planes/planes.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlanesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
