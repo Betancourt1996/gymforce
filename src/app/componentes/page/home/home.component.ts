4aacab6957d21ab0ec2183db548af06df6f83d80
Brayan Betancourt Ramirez <braybeta@espol.edu.ec>
2024-11-19 09:21:36 -0500

    posts

--- src/app/componentes/page/home/home.component.ts
+++ src/app/componentes/page/home/home.component.ts
@@ -4,11 +4,12 @@ import { PlanesComponent } from "../shared/planes/planes.component";
 import { CarruselComponent } from "../shared/carrusel/carrusel.component";
 import { GymsComponent } from "../shared/gyms/gyms.component";
 import { CarruselLeyendaComponent } from "../shared/carrusel-leyenda/carrusel-leyenda.component";
+import { PostComponent } from "../shared/post/post.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, PlanesComponent, CarruselComponent, GymsComponent, CarruselLeyendaComponent],
+  imports: [CommonModule, PlanesComponent, CarruselComponent, GymsComponent, CarruselLeyendaComponent, PostComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
