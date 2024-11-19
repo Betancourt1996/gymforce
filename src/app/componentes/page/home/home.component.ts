d8aa2d62cb80daa330490b47bd7550e59f0411ba
Brayan Betancourt Ramirez <braybeta@espol.edu.ec>
2024-11-18 22:09:00 -0500

    hasta citas

--- src/app/componentes/page/home/home.component.ts
+++ src/app/componentes/page/home/home.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { PlanesComponent } from "../shared/planes/planes.component";
+import { CarruselComponent } from "../shared/carrusel/carrusel.component";
+import { GymsComponent } from "../shared/gyms/gyms.component";
+import { CarruselLeyendaComponent } from "../shared/carrusel-leyenda/carrusel-leyenda.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, PlanesComponent],
+  imports: [CommonModule, PlanesComponent, CarruselComponent, GymsComponent, CarruselLeyendaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/componentes/page/shared/carrusel-leyenda/carrusel-leyenda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrusel-leyenda',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrusel-leyenda.component.html',
+  styleUrl: './carrusel-leyenda.component.scss'
+})
+export class CarruselLeyendaComponent {
+
+}
