7f2a6d97c7e5ac922033d00852c436c4ca52810f
Brayan Betancourt Ramirez <braybeta@espol.edu.ec>
2024-11-20 15:47:12 -0500

    hasta tabla

--- src/app/componentes/page/shared/carrusel-sede/carrusel-sede.component.ts
+++ src/app/componentes/page/shared/carrusel-sede/carrusel-sede.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HorarioGymComponent } from "../horario-gym/horario-gym.component";
 
 @Component({
   selector: 'app-carrusel-sede',
   standalone: true,
-  imports: [],
+  imports: [HorarioGymComponent],
   templateUrl: './carrusel-sede.component.html',
   styleUrl: './carrusel-sede.component.scss'
 })
---
+++ src/app/componentes/page/shared/horario-gym/horario-gym.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-horario-gym',
+  standalone: true,
+  imports: [],
+  templateUrl: './horario-gym.component.html',
+  styleUrl: './horario-gym.component.scss'
+})
+export class HorarioGymComponent {
+
+}
