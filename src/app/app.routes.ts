0889f415ed9e9845a4d3b1a5b4b82ad386252b2f
Brayan Betancourt Ramirez <braybeta@espol.edu.ec>
2024-11-21 14:24:37 -0500

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,16 +7,39 @@ import { ContactoComponent } from './componentes/page/contacto/contacto.componen
 import { SedeComponent } from './componentes/page/sede/sede.component';
 import { MembresiaComponent } from './componentes/page/membresia/membresia.component';
 import { BeneficiosComponent } from './componentes/page/beneficios/beneficios.component';
+import { LoginComponent } from './componentes/page/login/login.component';
+import { RegisterComponent } from './componentes/page/register/register.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { PerfilComponent } from './componentes/page/perfil/perfil.component';
+import { AuthGuard } from 'app/guards/auth.guard';
+
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'noticias', component: NoticiasComponent },
-  { path: 'noticias/:id', component: PostDetalleComponent },
-  { path: 'contacto', component: ContactoComponent },
-  { path: 'sede', component: SedeComponent },
-  { path: 'membresias', component: MembresiaComponent },
-  { path: 'beneficios', component: BeneficiosComponent },
-  { path: '**', redirectTo: '', pathMatch: 'full' },  
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'noticias', component: NoticiasComponent },
+      { path: 'noticias/:id', component: PostDetalleComponent },
+      { path: 'contacto', component: ContactoComponent },
+      { path: 'sede', component: SedeComponent },
+      { path: 'membresias', component: MembresiaComponent },
+      { path: 'beneficios', component: BeneficiosComponent },
+    ]
+  },
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'perfil', component: PerfilComponent, canActivate: [AuthGuard] }, 
+      { path: 'register', component: RegisterComponent },
+    ]
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
